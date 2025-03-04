609e78e5e61dd1fc78d46037e6e433efb3323da5
asfak00 <asfakahmed680@gmail.com>
2025-03-05 01:12:38 +0600

    update: added animation in the content navbar

--- src/Shared/Component/ContentNavbar.jsx
+++ src/Shared/Component/ContentNavbar.jsx
@@ -8,16 +8,30 @@ const ContentNavbar = ({contents, activeSection, width = '40%'}) => {
             </h2>
             <div style={{scrollbarWidth:'none'}} className='flex flex-col border-l max-h-[77vh] overflow-scroll dark:border-darkBorderColor border-border mt-4 w-[70%]'>
                 {contents?.map((item) => (
-                    <a
-                        key={item.id}
-                        href={item.href}
-                        className={`${
-                            activeSection === item.href.slice(1) &&
-                            '!text-primary bg-primary/5 dark:bg-slate-900 !border-primary'
-                        } text-[0.9rem] capitalize py-[8px] dark:text-darkSubTextColor/90 transition-all duration-300 text-text border-l-2 border-transparent pl-4`}
-                    >
-                        {item.title}
-                    </a>
+                    <div key={item.id} className="relative group">
+                        <div
+                            className={`
+                                absolute inset-y-0 left-0 bg-primary/5 dark:bg-slate-900 
+                                transition-all duration-[400ms] ease-linear
+                                ${activeSection === item.href.slice(1)
+                                ? 'w-full opacity-100'
+                                : 'w-0 opacity-0'}
+                            `}
+                        />
+                        <a
+                            href={item.href}
+                            className={`
+                                relative z-10 block text-[0.9rem] capitalize py-[8px] 
+                                dark:text-darkSubTextColor/90 transition-width duration-300 
+                                text-text border-l-2 border-transparent pl-4
+                                ${activeSection === item.href.slice(1)
+                                ? 'text-primary !border-primary'
+                                : ''}
+                            `}
+                        >
+                            {item.title}
+                        </a>
+                    </div>
                 ))}
             </div>
         </div>
