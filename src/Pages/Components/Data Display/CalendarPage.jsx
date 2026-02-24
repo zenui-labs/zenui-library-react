b22ecbd7ec72ffd2ec7bf4e307410ae1e2c5b029
mozaddedalfeshani <murad15-6047@diu.edu.bd>
2026-02-24 23:34:28 +0600

    feat: add Calendar and Toast pages with corresponding routes and contents

---
+++ src/Pages/Components/Data Display/CalendarPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// components
+import Calendar from "@components/Data Display/Calendar";
+import ContentPageLayout from "@shared/ContentPageLayout.jsx";
+
+const CalendarPage = () => {
+    return (
+        <ContentPageLayout>
+            <Calendar/>
+        </ContentPageLayout>
+    );
+};
+
+export default CalendarPage;
